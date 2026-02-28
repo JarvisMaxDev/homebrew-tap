b19bdb7434fe629c8c9aa10df695311ddec43f58
Jarvis <jarvis.max.dev@proton.me>
2026-02-28 15:06:55 +0000

    chore: update ReTyper cask to v0.6.0

--- Casks/retyper.rb
+++ Casks/retyper.rb
@@ -1,6 +1,6 @@
 cask "retyper" do
   version "0.6.0"
-  sha256 "a02dc68369adf67af9300a0f680905dab517a4c8151ab8bde99f7e22cb146a9d"
+  sha256 "9e3c9177f2ec5e60fbefa312c0810f85d67035347972444a8796d87a31bff4f5"
 
   url "https://github.com/JarvisMaxDev/ReTyper/releases/download/v#{version}/ReTyper-macOS-universal.dmg"
   name "ReTyper"
