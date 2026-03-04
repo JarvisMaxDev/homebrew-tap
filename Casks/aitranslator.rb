f9ec20f5ed699be06f7b204288d751821c44e468
JarvisMaxDev <jarvis.max.dev@proton.me>
2026-03-04 19:27:43 +0000

    chore: bump aitranslator to v1.11.1

--- Casks/aitranslator.rb
+++ Casks/aitranslator.rb
@@ -1,6 +1,6 @@
 cask "aitranslator" do
-  version "1.10.0"
-  sha256 "cd0ebea26cdbf7260d175b97e44171da1ee0f23d6856cf8747ee1f5f86bc5e42"
+  version "1.11.1"
+  sha256 "10ff30a82448c03d12972725cae5cf1dd0a1962a895c156c6fb7254c0b763336"
 
   url "https://github.com/JarvisMaxDev/AITranslator/releases/download/v#{version}/AITranslator.dmg"
   name "AI Translator"
